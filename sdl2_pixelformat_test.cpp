01c55778750af2ad9e72187ea7335b3adc77ffaf
Dimitris Panokostas <midwan@gmail.com>
2025-02-15 12:34:50 +0100

    refactor: loop through all texture formats for 10 times

--- sdl2_pixelformat_test.cpp
+++ sdl2_pixelformat_test.cpp
@@ -27,6 +27,10 @@
 #include <vector>
 #include <cstring>
 
+// Use LockTexture() + memcpy() + UnLockTexture() instead of UpdateTexture()
+bool useLocktexture = false;
+int loops = 10;
+
 int main( int argc, char** argv )
 {
     SDL_Init( SDL_INIT_EVERYTHING );
@@ -49,7 +53,7 @@ int main( int argc, char** argv )
     SDL_RendererInfo info;
     SDL_GetRendererInfo( renderer, &info );
     std::cout << "Renderer name: " << info.name << std::endl;
-    std::cout << "Texture formats: " << std::endl;
+    std::cout << "Available Texture formats for renderer: " << std::endl;
     for( Uint32 i = 0; i < info.num_texture_formats; i++ )
     {
         std::cout << SDL_GetPixelFormatName( info.texture_formats[i] ) << std::endl;
@@ -57,104 +61,111 @@ int main( int argc, char** argv )
 
     const unsigned int texWidth = 1024;
     const unsigned int texHeight = 1024;
-    SDL_Texture* texture = SDL_CreateTexture
-        (
-        renderer,
-        SDL_PIXELFORMAT_RGBA32,
-        SDL_TEXTUREACCESS_STREAMING,
-        texWidth, texHeight
+
+    for (Uint32 i = 0; i < info.num_texture_formats; i++)
+    {
+        auto pixelFormat = info.texture_formats[i];
+        std::cout << "Testing Pixel format: " << SDL_GetPixelFormatName(pixelFormat) << std::endl;
+
+        SDL_Texture* texture = SDL_CreateTexture(
+            renderer,
+            pixelFormat,
+            SDL_TEXTUREACCESS_STREAMING,
+            texWidth, texHeight
         );
 
-    std::vector< unsigned char > pixels( texWidth * texHeight * 4, 0 );
+        std::vector<unsigned char> pixels(texWidth * texHeight * 4, 0);
 
-    SDL_Event event;
-    bool running = true;
-    bool useLocktexture = false;
-    
-    unsigned int frames = 0;
-    Uint64 start = SDL_GetPerformanceCounter();
-
-    while( running )
-    {
-
-        SDL_SetRenderDrawColor( renderer, 0, 0, 0, SDL_ALPHA_OPAQUE );
-        SDL_RenderClear( renderer );
-
-        while( SDL_PollEvent( &event ) )
+        for (int test = 0; test < loops; test++)
         {
-            if( ( SDL_QUIT == event.type ) ||
-                ( SDL_KEYDOWN == event.type && SDL_SCANCODE_ESCAPE == event.key.keysym.scancode ) )
+            SDL_Event event;
+            bool running = true;
+            unsigned int frames = 0;
+            Uint64 start = SDL_GetPerformanceCounter();
+
+            std::cout << "Using " << (useLocktexture ? "SDL_LockTexture() + memcpy()" : "SDL_UpdateTexture()") << std::endl;
+
+            while (running)
             {
-                running = false;
-                break;
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+                SDL_RenderClear(renderer);
+
+                while (SDL_PollEvent(&event))
+                {
+                    if ((SDL_QUIT == event.type) ||
+                        (SDL_KEYDOWN == event.type && SDL_SCANCODE_ESCAPE == event.key.keysym.scancode))
+                    {
+                        running = false;
+                        break;
+                    }
+                    if (SDL_KEYDOWN == event.type && SDL_SCANCODE_L == event.key.keysym.scancode)
+                    {
+                        useLocktexture = !useLocktexture;
+                        std::cout << "Using " << (useLocktexture ? "SDL_LockTexture() + memcpy()" : "SDL_UpdateTexture()") << std::endl;
+                    }
+                }
+
+                for (unsigned int i = 0; i < 1000; i++)
+                {
+                    const unsigned int x = rand() % texWidth;
+                    const unsigned int y = rand() % texHeight;
+
+                    const unsigned int offset = (texWidth * 4 * y) + x * 4;
+                    pixels[offset + 0] = rand() % 256;
+                    pixels[offset + 1] = rand() % 256;
+                    pixels[offset + 2] = rand() % 256;
+                    pixels[offset + 3] = SDL_ALPHA_OPAQUE;
+                }
+
+                if (useLocktexture)
+                {
+                    unsigned char* lockedPixels = nullptr;
+                    int pitch = 0;
+                    SDL_LockTexture(
+                        texture,
+                        NULL,
+                        reinterpret_cast<void**>(&lockedPixels),
+                        &pitch
+                    );
+                    std::memcpy(lockedPixels, pixels.data(), pixels.size());
+                    SDL_UnlockTexture(texture);
+                }
+                else
+                {
+                    SDL_UpdateTexture(
+                        texture,
+                        NULL,
+                        pixels.data(),
+                        texWidth * 4
+                    );
+                }
+
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
+
+                frames++;
+                const Uint64 end = SDL_GetPerformanceCounter();
+                const static Uint64 freq = SDL_GetPerformanceFrequency();
+                const double seconds = (end - start) / static_cast<double>(freq);
+                if (seconds > 2.0)
+                {
+                    std::cout
+                        << frames << " frames in "
+                        << std::setprecision(1) << std::fixed << seconds << " seconds = "
+                        << std::setprecision(1) << std::fixed << frames / seconds << " FPS ("
+                        << std::setprecision(3) << std::fixed << (seconds * 1000.0) / frames << " ms/frame)"
+                        << std::endl;
+                    start = end;
+                    frames = 0;
+                }
             }
-            if( SDL_KEYDOWN == event.type && SDL_SCANCODE_L == event.key.keysym.scancode )
-            {
-                useLocktexture = !useLocktexture;
-                std::cout << "Using " << ( useLocktexture ? "SDL_LockTexture() + memcpy()" : "SDL_UpdateTexture()" ) << std::endl;
-            }
-        }
-        
-        // splat down some random pixels
-        for( unsigned int i = 0; i < 1000; i++ )
-        {
-            const unsigned int x = rand() % texWidth;
-            const unsigned int y = rand() % texHeight;
 
-            const unsigned int offset = ( texWidth * 4 * y ) + x * 4;
-            pixels[ offset + 0 ] = rand() % 256;        // b
-            pixels[ offset + 1 ] = rand() % 256;        // g
-            pixels[ offset + 2 ] = rand() % 256;        // r
-            pixels[ offset + 3 ] = SDL_ALPHA_OPAQUE;    // a
-        }
-
-        if( useLocktexture )
-        {
-            unsigned char* lockedPixels = nullptr;
-            int pitch = 0;
-            SDL_LockTexture
-                (
-                texture,
-                NULL,
-                reinterpret_cast< void** >( &lockedPixels ),
-                &pitch
-                );
-            std::memcpy( lockedPixels, pixels.data(), pixels.size() );
-            SDL_UnlockTexture( texture );
-        }
-        else
-        {
-            SDL_UpdateTexture
-                (
-                texture,
-                NULL,
-                pixels.data(),
-                texWidth * 4
-                );
-        }
-
-        SDL_RenderCopy( renderer, texture, NULL, NULL );
-        SDL_RenderPresent( renderer );
-        
-        frames++;
-        const Uint64 end = SDL_GetPerformanceCounter();
-        const static Uint64 freq = SDL_GetPerformanceFrequency();
-        const double seconds = ( end - start ) / static_cast< double >( freq );
-        if( seconds > 2.0 )
-        {
-            std::cout
-                << frames << " frames in "
-                << std::setprecision(1) << std::fixed << seconds << " seconds = "
-                << std::setprecision(1) << std::fixed << frames / seconds << " FPS ("
-                << std::setprecision(3) << std::fixed << ( seconds * 1000.0 ) / frames << " ms/frame)"
-                << std::endl;
-            start = end;
-            frames = 0;
+            SDL_DestroyTexture(texture);
         }
     }
 
-    SDL_DestroyRenderer( renderer );
-    SDL_DestroyWindow( window );
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
